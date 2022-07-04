4189d068c9b07a3472dabebe3f4bb859ef297ca9
Artkabis <artkabis@users.noreply.github.com>
2022-07-04 17:17:09 +0200

    update : Woocommerce pagination des produits

--- Solocal tools, tips & fix/woocommerce.js
+++ Solocal tools, tips & fix/woocommerce.js
@@ -62,8 +62,14 @@ $('.woocommerce').append('<button class="rightBtn">&gt;</button>')
 
 
 /************************** Woocommerce pagination des produits et gestion du nombre d'isotope affichable *****/
-
 if($('.woocommerce').length ){
+    const nbElementView =8;//Nombre de produits par page
+    
+    //Calcul du nombre d'items et de pages
+    function recalc($woo){
+        const items = Number($woo.find('.isotope-item').length);
+         return {'nbItems':items,'nbPages': Math.ceil(items /nbElementView)};
+    }
     function createNav($woo,id){
         const NAV = `<div class="vc_row wpb_row vc_inner vc_row-fluid"><div class="wpb_column vc_column_container vc_col-sm-4"><div class="vc_column-inner"><div class="wpb_wrapper"><div class="vc_btn3-container vc_btn3-inline" id="btnPrev-${id}">
         	<button class="vc_general vc_btn3 vc_btn3-size-md vc_btn3-shape-rounded vc_btn3-style-modern vc_btn3-color-grey">page précédente</button>	</div>
@@ -78,27 +84,30 @@ if($('.woocommerce').length ){
         	<button class="vc_general vc_btn3 vc_btn3-size-md vc_btn3-shape-rounded vc_btn3-style-modern vc_btn3-color-grey">page suivante</button>	</div>
         </div></div></div></div>`;
         console.log($woo,id);
-        if($woo.find('.isotope-item').length>20){
+        if($woo.find('.isotope-item').length>nbElementView){
             
         $woo.after(NAV);
-            const nbElementView = 20;
+            
             //console.log('page trouvée');
-            var counter,nbPage;
+            var counter,nbPage,nbItems;
             function init(){
                 counter = 1;
-                $('#nbPage-'+id+' p').text(1);
+                
                 $woo.find('.isotope-item').fadeOut(0);
                 $woo.find('.isotope-item:nth-child(-n+'+nbElementView+')').fadeIn(0);
-                const nbItems = Number($woo.find('.isotope-item').length);
-                nbPages =   Math.ceil(nbItems /nbElementView);
-                //console.log('nb page : ',nbPages);
-                //console.log('nb item : ',nbItems);
+                nbItems = recalc($woo).nbItems;
+                nbPages = recalc($woo).nbPages;
+                $('#nbPage-'+id+' p').text('1/'+nbPages);
+                console.log('nb page : ',nbPages);
+                console.log('nb item : ',nbItems);
             }
             init();
             function wooPage($nb){
-                $('.nbPage p').text($nb);
-                //console.log('nb : ',$nb);
-                targetNb = ($nb*2)*10;
+                nbItems = recalc($woo).nbItems;
+                nbPages = recalc($woo).nbPages;
+                $('#nbPage-'+id+' p').text($nb+'/'+nbPages);
+                console.log('nb : ',$nb);
+                targetNb = ($nb*nbElementView);//*10;
                 targetEnd = Number(targetNb)+1;
                 targetBefore = targetNb-nbElementView;
                 //console.log('a afficher à partir de : ',targetNb, ' caché avant : ',targetBefore,'  fin à cacher : ',targetEnd);
@@ -109,21 +118,18 @@ if($('.woocommerce').length ){
             
             $('#btnNext-'+id).on('click',function(me){
                 counter++;
-                //console.log('click next : ',$(this).attr('id'),counter);
+                console.log('click next : ',$(this).attr('id'),counter);
                 if(counter>nbPages) {
                     wooPage(1);
                     counter=1;
                    }else{ wooPage(counter);}
-                $('#nbPage-'+id+' p').text(counter);
             });
             $('#btnPrev-'+id).on('click',function(me){
                 counter--;
-                //console.log('click prev : ',$(this).attr('id'),counter);
                if(counter<1) {
                     wooPage(nbPages);
                     counter=nbPages;
                    }else{ wooPage(counter);}
-                   $('#nbPage-'+id+' p').text(counter);
             });
         }
     }
