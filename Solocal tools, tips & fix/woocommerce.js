897364dcfa628d49dac05345ec3c21149f065ad6
Artkabis <artkabis@users.noreply.github.com>
2022-07-04 14:08:33 +0200

    Add Woocommerce pagination isotope

--- Solocal tools, tips & fix/woocommerce.js
+++ Solocal tools, tips & fix/woocommerce.js
@@ -56,3 +56,78 @@ $('.woocommerce').append('<button class="rightBtn">&gt;</button>')
         }
     });
 })(jQuery);
+
+
+
+
+
+/************************** Woocommerce pagination des produits et gestion du nombre d'isotope affichable *****/
+
+if($('.woocommerce').length ){
+    function createNav($woo,id){
+        const NAV = `<div class="vc_row wpb_row vc_inner vc_row-fluid"><div class="wpb_column vc_column_container vc_col-sm-4"><div class="vc_column-inner"><div class="wpb_wrapper"><div class="vc_btn3-container vc_btn3-inline" id="btnPrev-${id}">
+        	<button class="vc_general vc_btn3 vc_btn3-size-md vc_btn3-shape-rounded vc_btn3-style-modern vc_btn3-color-grey">page précédente</button>	</div>
+        </div></div></div><div class="wpb_column vc_column_container vc_col-sm-4"><div class="vc_column-inner"><div class="wpb_wrapper">
+        	<div class="wpb_text_column wpb_content_element " id="nbPage-${id}">
+        		<div class="wpb_wrapper">
+        			<p>0</p>
+        
+        		</div>
+        	</div>
+        </div></div></div><div class="wpb_column vc_column_container vc_col-sm-4"><div class="vc_column-inner"><div class="wpb_wrapper"><div class="vc_btn3-container vc_btn3-inline" id="btnNext-${id}">
+        	<button class="vc_general vc_btn3 vc_btn3-size-md vc_btn3-shape-rounded vc_btn3-style-modern vc_btn3-color-grey">page suivante</button>	</div>
+        </div></div></div></div>`;
+        console.log($woo,id);
+        if($woo.find('.isotope-item').length>20){
+            
+        $woo.after(NAV);
+            const nbElementView = 20;
+            //console.log('page trouvée');
+            var counter,nbPage;
+            function init(){
+                counter = 1;
+                $('#nbPage-'+id+' p').text(1);
+                $woo.find('.isotope-item').fadeOut(0);
+                $woo.find('.isotope-item:nth-child(-n+'+nbElementView+')').fadeIn(0);
+                const nbItems = Number($woo.find('.isotope-item').length);
+                nbPages =   Math.ceil(nbItems /nbElementView);
+                //console.log('nb page : ',nbPages);
+                //console.log('nb item : ',nbItems);
+            }
+            init();
+            function wooPage($nb){
+                $('.nbPage p').text($nb);
+                //console.log('nb : ',$nb);
+                targetNb = ($nb*2)*10;
+                targetEnd = Number(targetNb)+1;
+                targetBefore = targetNb-nbElementView;
+                //console.log('a afficher à partir de : ',targetNb, ' caché avant : ',targetBefore,'  fin à cacher : ',targetEnd);
+                $woo.find('.isotope-item:nth-child(-n+'+targetNb+')').fadeIn(0);
+                ($nb>1)&&$($woo).find('.isotope-item:nth-child(-n+'+targetBefore+')').fadeOut(0);
+                $woo.find('.isotope-item:nth-child(n+'+targetEnd+')').fadeOut(0);
+            }
+            
+            $('#btnNext-'+id).on('click',function(me){
+                counter++;
+                //console.log('click next : ',$(this).attr('id'),counter);
+                if(counter>nbPages) {
+                    wooPage(1);
+                    counter=1;
+                   }else{ wooPage(counter);}
+                $('#nbPage-'+id+' p').text(counter);
+            });
+            $('#btnPrev-'+id).on('click',function(me){
+                counter--;
+                //console.log('click prev : ',$(this).attr('id'),counter);
+               if(counter<1) {
+                    wooPage(nbPages);
+                    counter=nbPages;
+                   }else{ wooPage(counter);}
+                   $('#nbPage-'+id+' p').text(counter);
+            });
+        }
+    }
+    $('.woocommerce').each(function(i,t){
+        createNav($(this),i);
+    });
+}
