b7fc31f0fb9092c5ca3a24f9595b97aeb17f3e50
Artkabis <artkabis@users.noreply.github.com>
2022-08-05 17:21:23 +0200

    Encode mail in DOM web page & use
    Secure your mail vs bot crawl for your website

---
+++ Solocal tools, tips & fix/protectEmailEncode.js
@@ -0,0 +1,21 @@
+
+// Élément à intégrer en html, vous pouvez utiliser la fonction reverseEmail ci-dessous avec le each afin de récupérer vos e-mails en reverse
+//<span class="encodemail">moc.eniamod@tcatnoc</span>
+(function($){
+    function reverseEmail($mail){
+        return ($mail) && $mail.split("").reverse().join("")
+    }
+    //Ceci permet de récupérer les adresses mails inversé (avant intégration)
+    $.each(["monadress1@gmail.com","monadress2@gmail.com","monadress3@gmail.com"],function(i,t){
+        console.log(reverseEmail(t));
+    });
+    //Les e-mails seront désormais visibles dans leur format initial, sans que ce soit le cas dans le DOM
+    $('.encodemail').each(function(i,t){
+        $(this).css({unicodeBidi:"bidi-override",direction:"rtl",cursor:"pointer"});
+    });
+    //Reconstruction de l'e-mail uniquement après click, puis ouverture du mailto
+    $('.encodemail').on('click',function(){
+        const mailReverse = reverseEmail($(this).text());
+        location.href = "mailto:"+mailReverse+"?cc=lemailencopie@gmail.com&subject=Prise de contact depuis monsiteprivilege.fr&body=Ce mail concerne votre site SPR";
+    });
+})(jQuery)
