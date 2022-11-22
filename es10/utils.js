d3b5c7f4cc3c2aad63e7503a9e87ebb28843859b
Artkabis <artkabis@users.noreply.github.com>
2022-11-22 14:42:26 +0100

    Update utils.js

--- es10/utils.js
+++ es10/utils.js
@@ -1,2 +1,2 @@
-const funcMod1 = ()=> console.log('funcMod1 -> arg : ', ...argument, ' --- arg length : ', argument.length);
-const funcMod2 = ()=> console.log('funcMod2 -> arg : ', ...argument, ' --- arg length : ', argument.length);
+const funcMod1 = ()=> console.log('funcMod1 -> arg : ', ...arguments, ' --- arg length : ', arguments.length);
+const funcMod2 = ()=> console.log('funcMod2 -> arg : ', ...arguments, ' --- arg length : ', arguments.length);
