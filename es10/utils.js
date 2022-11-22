d3225e7c509bd145f3239b893550ed3fd29fe891
Artkabis <artkabis@users.noreply.github.com>
2022-11-22 14:50:42 +0100

    Update utils.js

--- es10/utils.js
+++ es10/utils.js
@@ -1,2 +1,7 @@
-export function funcMod1 (...arguments){ console.log('funcMod1 -> arg : ', arguments, ' --- arg length : ', arguments.length); }
+/*export function funcMod1 (...arguments){ console.log('funcMod1 -> arg : ', arguments, ' --- arg length : ', arguments.length); }
 export function funcMod2 (...arguments){ console.log('funcMod2 -> arg : ', arguments, ' --- arg length : ', arguments.length); }
+*/
+
+export function getMessage() {
+  return "Hello world";
+}
