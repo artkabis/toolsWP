ed0e1f323ecbe669d9dcde5de74412622955bb19
Artkabis <artkabis@users.noreply.github.com>
2022-11-22 15:25:03 +0100

    Update utils.js

--- es10/utils.js
+++ es10/utils.js
@@ -1,11 +1,6 @@
 "use strict";
 
-export function getMessage() {
-  return "Hello world";
-}
-export function funcMod1 (...args) {
- return String('funcMod1 -> arg : '+ args+ ' --- arg length : '+ args.length); 
-}
-export function funcMod2 (...args) {
-  return String('funcMod2 -> arg : '+ args+ ' --- arg length : '+ args.length); 
-}
+export function randomMM (min,max){return Math.floor(Math.random() * (max - min + 1)) + min}
+export function isNumber(n){ return !isNaN(parseFloat(n)) && isFinite(n)}
+export function toNumber(s){ return (s) && +s}
+export function noDuplcaArray(a){ return [...new Set(a)]}
