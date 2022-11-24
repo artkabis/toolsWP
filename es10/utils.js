9d6efb47a4f888372b4ebb7753db212c8360d894
Artkabis <artkabis@users.noreply.github.com>
2022-11-24 16:59:52 +0100

    Update utils.js

--- es10/utils.js
+++ es10/utils.js
@@ -7,3 +7,4 @@ export function noDuplcaArray(a){ return [...new Set(a)]}
 export function onlyValidValueArray(a){a.filter(Boolean)}
 export function generateToken(){const shuffle = ()=>Math.random().toString(36).substr(2);let tokenA = [];[0,0,0].forEach((t)=>tokenA.push(shuffle()+shuffle()));return String(tokenA.join(''));}
 export function shuffleArray(arr){let tabRange, randomiz;(arr && Array.isArray(arr)) && (tabRange = [...new Set(arr)].sort(),randomiz =  tabRange.sort(()=> Math.random()-0.5));return randomiz}
+export function maxValueArray(arr){return arr.reduce((a,b) => a>b?a:b)}
