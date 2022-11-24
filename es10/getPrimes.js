ecee7a2fceed9c92d6291abecdcdcf3d0cecbac4
Artkabis <artkabis@users.noreply.github.com>
2022-11-24 16:02:14 +0100

    Create getPrimes.js

---
+++ es10/getPrimes.js
@@ -0,0 +1,19 @@
+// getPrimes.js
+/**
+ * Returns a list of prime numbers that are smaller than `max`.
+ */
+export function getPrimes(max) {
+  const isPrime = Array.from({ length: max }, () => true);
+  isPrime[0] = isPrime[1] = false;
+  isPrime[2] = true;
+  for (let i = 2; i * i < max; i++) {
+    if (isPrime[i]) {
+      for (let j = i ** 2; j < max; j += i) {
+        isPrime[j] = false;
+      }
+    }
+  }
+  return [...isPrime.entries()]
+    .filter(([, isPrime]) => isPrime)
+    .map(([number]) => number);
+}
