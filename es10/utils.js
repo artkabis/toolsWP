e1ffa6042dbe9b9643067c8a6ed8bd1c912a2baa
Artkabis <artkabis@users.noreply.github.com>
2022-11-24 17:19:27 +0100

    Update utils.js

--- es10/utils.js
+++ es10/utils.js
@@ -8,3 +8,5 @@ export function onlyValidValueArray(a){a.filter(Boolean)}
 export function generateToken(){const shuffle = ()=>Math.random().toString(36).substr(2);let tokenA = [];[0,0,0].forEach((t)=>tokenA.push(shuffle()+shuffle()));return String(tokenA.join(''));}
 export function shuffleArray(arr){let tabRange, randomiz;(arr && Array.isArray(arr)) && (tabRange = [...new Set(arr)].sort(),randomiz =  tabRange.sort(()=> Math.random()-0.5));return randomiz}
 export function maxValueArray(arr){return arr.reduce((a,b) => a>b?a:b)}
+export function ascii2hex(str){var arr1 = [];for (var n = 0, l = str.length; n < l; n ++){var hex = Number(str.charCodeAt(n)).toString(16);arr1.push(hex);}return arr1.join('');}
+export function hex2ascii(str1){var hex  = str1.toString();var str = '';for (var n = 0; n < hex.length; n += 2) {str += String.fromCharCode(parseInt(hex.substr(n, 2), 16));}return str;}
