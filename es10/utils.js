6f68fe3c1bc0c9b87f2d5eb67493e6f548c89075
Artkabis <artkabis@users.noreply.github.com>
2022-11-24 15:43:57 +0100

    Add generateToken

--- es10/utils.js
+++ es10/utils.js
@@ -5,3 +5,5 @@ export function isNumber(n){ return !isNaN(parseFloat(n)) && isFinite(n)}
 export function toNumber(s){ return (s) && +s}
 export function noDuplcaArray(a){ return [...new Set(a)]}
 export function onlyValidValueArray(a){a.filter(Boolean)}
+export function generateToken(){const shuffle = ()=>Math.random().toString(36).substr(2);let tokenA = [];[0,0,0].forEach((t)=>tokenA.push(shuffle()+shuffle()));return String(tokenA.join(''));
+}
