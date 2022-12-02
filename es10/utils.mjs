d6cbb834bb88dea3ed100db2e7cd1c6c7025949f
Artkabis <artkabis@users.noreply.github.com>
2022-12-02 11:43:36 +0100

    Update utils.mjs

--- es10/utils.mjs
+++ es10/utils.mjs
@@ -7,8 +7,9 @@ export const isNotUndefined = (n) => n === undefined ? false : true;
 export const toNumber = (s) =>  (s) && +s;
 export const isEven = (n) => n % 2 === 0;
 export const isOdd = (n) => n % 2 === 1;
+export const convertDate2fr = (el) => el.split('-').reverse(el).join('/');
 export const noDuplcaArray = (a) =>  [...new Set(a)];
-export const  onlyValidValueArray = (a) =>a.filter(Boolean);
+export const onlyValidValueArray = (a) =>a.filter(Boolean);
 export const generateToken = () =>{const shuffle = ()=>Math.random().toString(36).substr(2);let tokenA = [];[0,0,0].forEach((t)=>tokenA.push(shuffle()+shuffle()));return String(tokenA.join(''));}
 export const shuffleArray = (arr) => {let tabRange, randomiz;(arr && Array.isArray(arr)) && (tabRange = [...new Set(arr)].sort(),randomiz =  tabRange.sort(()=> Math.random()-0.5));return randomiz}
 export const maxValueArray = (arr) => arr.reduce((a,b) => a>b?a:b);
