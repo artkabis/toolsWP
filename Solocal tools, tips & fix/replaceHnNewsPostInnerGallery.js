70ce57fbf8217a1af3bc0624fdf1f91deb9c8e9b
Artkabis <artkabis@hotmail.fr>
2022-12-22 12:25:40 +0100

    Create replaceHnNewsPostInnerGallery.js

---
+++ Solocal tools, tips & fix/replaceHnNewsPostInnerGallery.js
@@ -0,0 +1,22 @@
+//Fonction permettant d'écouter la mise en place d'un élément dans le DOM et de lancer une fonction 
+const onDomElementIsReady = (elementToWatch)=> {
+        return new Promise((res, rej)=> {
+          let idInterval = setInterval(()=> {
+            if($(elementToWatch).length > 0) {
+              clearInterval(idInterval); //suppression de l'interval
+              res($(elementToWatch)); //resolution de promise (une fois la pile d'événements terminée)            
+            }
+          },100);
+        });
+      };
+
+//Fix H4 news
+$(window).on('load',function(){
+    if(window.location.pathname.includes('/actualites/')){
+        onDomElementIsReady('.vc_grid h4').then(el =>{
+           $('.vc_grid h4').each(function(i,t){
+                $(this).replaceWith(`<span style="font-size:1.2em;margin-top:10px">${t.innerText}</span>`);
+           });
+        });
+    }
+});
