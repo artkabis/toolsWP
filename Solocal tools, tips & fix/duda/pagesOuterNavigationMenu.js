e8bdce3f3049c3d13d43fcc1b597413c4f9b41b9
Artkabis <artkabis@hotmail.fr>
2022-12-27 17:34:30 +0100

    Create pagesOuterNavigationMenu.js
    Renvoi l'ensemble des pages n'étant pas dans le menu de navigation Duda sous forme d'un tableau : 
    (index), title, alias, path, inNavigation, subNav

---
+++ Solocal tools, tips & fix/duda/pagesOuterNavigationMenu.js
@@ -0,0 +1,8 @@
+const getOuterNav=()=>{
+        const menuJson = JSON.parse(atob(Parameters.NavItems));
+        const subNavOk = menuJson.filter((t)=>t.subNav.length>0);
+        const nv1OutNav = menuJson.filter((t)=>t.inNavigation===false);
+        let nv2Temp = {};
+        const nv2OutNav = (subNavOk) && subNavOk.filter((s)=>s.inNavigation==false);
+    return (nv2OutNav>0) ? {nv1OutNav,nv2OutNav} : nv1OutNav
+}
