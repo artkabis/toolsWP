308723ef852f695bb5e80b2d420d016a92f61407
Artkabis <artkabis@hotmail.fr>
2022-12-27 17:37:30 +0100

    Update DudaInfosProductSite.js

--- Solocal tools, tips & fix/duda/DudaInfosProductSite.js
+++ Solocal tools, tips & fix/duda/DudaInfosProductSite.js
@@ -4,8 +4,10 @@ const planId = dmAPI.getSitePlanID();
 const device = dmAPI.getCurrentDeviceType();
 dmAPI.runOnReady('DudaReadyDOM',funcReadyDomDM);
 
-
-
 function funcReadyDomDM(){
     console.log('Duda ready - infos -> siteName:',siteName,' - externalId : ',externalId,' - planId : ',planId,' - device : ',device);
 }
+
+//Bookmark
+javascript:(()=>console.table({'gamme':Parameters.ExternalUid.split('|')[0], 'epj':Parameters.ExternalUid.split('|')[1],'url onLive':Parameters.HomeUrl,'prepub':Parameters.AccountUUID,'device':window._currentDevice,' ID plan : ',dmAPI.getSitePlanID()}))()
+
