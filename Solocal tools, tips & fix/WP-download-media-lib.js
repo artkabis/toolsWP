14170d80b0a909c8134edfc2ca53ccc74291c692
Artkabis <artkabis@hotmail.fr>
2023-01-12 18:28:25 +0100

    Update WP-download-media-lib.js

--- Solocal tools, tips & fix/WP-download-media-lib.js
+++ Solocal tools, tips & fix/WP-download-media-lib.js
@@ -2,25 +2,29 @@
 * -- @limit_nb_img (Number) -> Nombre d'image maximum à télécharger
 * -- @start_img (Number) -> Débuter le téléchargement à partir de la x image (null si désactivé)
 ***/
-var cmp=0;
-const limit_nb_img = 10,start_img = 5;
-async function downloadImage(imageSrc,name) {
-    if(cmp<limit_nb_img){
+
+const limit_nb_img = 20,start_img = 2;
+let cmp = start_img;
+async function downloadImage(imageSrc,name, id) {
       const image = await fetch(imageSrc)
       const imageBlog = await image.blob()
       const imageURL = URL.createObjectURL(imageBlog)
       const link = document.createElement('a')
       link.href = imageURL
+      link.id='imgDownload'+id;
       link.download = name
+        console.log(link);
       document.body.appendChild(link)
-      link.click()
-      document.body.removeChild(link)
-    }
+        if(cmp < limit_nb_img){
+          link.click()
+          //document.body.removeChild(link)
+        }
     cmp++;
 }
 const imgs = (document.querySelectorAll('.save-ready').length) ? document.querySelectorAll('.save-ready img') : document.querySelectorAll('td[data-colname="Fichier"] img');
 const linksArray = (start_img) ? Array.from(imgs).slice(start_img,Array.from(imgs).length) : Array.from(imgs);
-linksArray.forEach(linkEl => {
+linksArray.forEach((linkEl,i) => {
+    console.log('i : ',i, 'linkEl : ',linkEl);
     const img = linkEl.getAttribute("src");
     const ext = img.substring(img.lastIndexOf('.'),img.length);
     const last2 = img.lastIndexOf('/');
@@ -36,5 +40,5 @@ linksArray.forEach(linkEl => {
         finalImg = img;
     }
   console.log('src : ',img,'   name : ',name,'  link final image : ',finalImg,'  extension : ',ext);
-  downloadImage(finalImg,name)
+  downloadImage(finalImg,name,i)
 });
