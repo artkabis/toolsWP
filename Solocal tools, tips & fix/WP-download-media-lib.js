a5332a0e5244faf6553226926cc1117300a0da02
Artkabis <artkabis@hotmail.fr>
2023-01-13 09:52:37 +0100

    Update WP-download-media-lib.js

--- Solocal tools, tips & fix/WP-download-media-lib.js
+++ Solocal tools, tips & fix/WP-download-media-lib.js
@@ -2,7 +2,8 @@
 * -- @max_img (Number) -> Nombre d'images maximums à télécharger
 * -- @start_img (Number) -> Débuter le téléchargement à partir de la x image
 ***/
-const max_img = 25, start_img = 5;//Ici téléchargement à partir de la cinquième image et jusqu'à la vingt-cinquième
+
+const max_img = 25, start_img = 5;//params de délimitation
 function pause(msec) {
     return new Promise(
         (resolve, reject) => {
@@ -14,15 +15,17 @@ let loop = start_img;
 function convertirHrefEnBlobEtTelecharger(href,name,cmp) {
     // Faire une requête AJAX pour récupérer le contenu du fichier
     const isReady = (loop<=max_img &&  loop >= start_img && String(href).includes('site-privilege'));
-    console.log('loop<=max_img : ',loop<=max_img,'    start_img>= loop : ',start_img>= loop,'     href.includes("site-privilege") :',String(href).includes('site-privilege'));
+    console.log('loop<=max_img : ',loop<=max_img,'    loop >= start_img : ',loop >= start_img,'     href.includes("site-privilege") :',String(href).includes('site-privilege'));
     console.log('*************************** isReady : ',isReady);
     if(isReady){
         const xhr = new XMLHttpRequest();
         xhr.open('GET', href, true);
         xhr.responseType = 'blob';
         xhr.onload = function(e) {
+            
             // Récupérer le blob
-            const blob = xhr.response;
+            var blob = xhr.response;
+            
             // Créer un lien pour télécharger le fichier
             const link = document.createElement('a');
             link.href = window.URL.createObjectURL(blob);
@@ -45,7 +48,7 @@ linksArray.forEach((linkEl,i) => {
     const last2 = img.lastIndexOf('/');
     var name="",finalImg="";
     if(img.match(/\d{3}x\d{3}/m) || img.match(/\d{2}x\d{2}/m) ){
-        console.log(redimensionnement détecté : ',img);
+        console.log('Redimentionnement détecté : ',img);
       const last = img.lastIndexOf('-');
       finalImg = window.location.origin+img.substring(0,last)+ext;
       name = String(img.substring(last2,img.length).split(ext)[0].substring(0,img.substring(last2,img.length).split(ext)[0].lastIndexOf('-'))).replace('/','');
@@ -55,16 +58,14 @@ linksArray.forEach((linkEl,i) => {
         finalImg = img;
     }
   console.log('src : ',img,'   name : ',name,'  link final image : ',finalImg,'  extension : ',ext);
-  //convertirHrefEnBlobEtTelecharger(finalImg,name,i)
-    jsonImg.push({img:finalImg,name:name,cmp:i});
+  jsonImg.push({img:finalImg,name:name,cmp:i});
 });
 console.log('_______________ jsonImg : ',jsonImg);
 async function downloadAll(elements) {
     var count = 0;
     for (var e in elements) {
-
-        convertirHrefEnBlobEtTelecharger(elements[e].img,elements[e].name, elements[e].cmp); // your custom download code here, click or whatever
-
+        convertirHrefEnBlobEtTelecharger(elements[e].img,elements[e].name, elements[e].cmp); // Lancement de la construction des img et du download.
+        //Gestion de la pause de 1 seconde toutes les 10 itérations
         if (++count >= 10) {
             await pause(1000);
             count = 0;
