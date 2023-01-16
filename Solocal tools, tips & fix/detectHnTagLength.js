e143d52231aa782df428042cc12e7830b17cf837
Artkabis <artkabis@hotmail.fr>
2023-01-16 11:33:02 +0100

    Refactoring : one loop for all Hn

--- Solocal tools, tips & fix/detectHnTagLength.js
+++ Solocal tools, tips & fix/detectHnTagLength.js
@@ -1,38 +1,6 @@
 javascript:(()=>{
-  var hn = document.getElementsByTagName('h1');
-  for (var i = 0; i < hn.length; i++) {
-    var hnLength = document.createElement('div');
-    hnLength.innerHTML = `<span style="color:red;background:black;padding:15ps">${hn[i].tagName} : ${hn[i].innerText.length} caractères</span>`;
-    hn[i].appendChild(hnLength);
-  }
-  var hn = document.getElementsByTagName('h2');
-  for (var i = 0; i < hn.length; i++) {
-    var hnLength = document.createElement('div');
-    hnLength.innerHTML = `<span style="color:red;background:black;padding:15ps">${hn[i].tagName} : ${hn[i].innerText.length} caractères</span>`;
-   hn[i].appendChild(hnLength);
-  }
-  var hn = document.getElementsByTagName('h3');
-  for (var i = 0; i < hn.length; i++) {
-    var hnLength = document.createElement('div');
-    hnLength.innerHTML = `<span style="color:red;background:black;padding:15ps">${hn[i].tagName} : ${hn[i].innerText.length} caractères</span>`;
-    hn[i].appendChild(hnLength);
-  }
-  var hn = document.getElementsByTagName('h4');
-  for (var i = 0; i < hn.length; i++) {
-    var hnLength = document.createElement('div');
-    hnLength.innerHTML = `<span style="color:red;background:black;padding:15ps">${hn[i].tagName} : ${hn[i].innerText.length} caractères</span>`;
-    hn[i].appendChild(hnLength);
-  }
-  var hn = document.getElementsByTagName('h5');
-  for (var i = 0; i < hn.length; i++) {
-    var hnLength = document.createElement('div');
-    hnLength.innerHTML = `<span style="color:red;background:black;padding:15ps">${hn[i].tagName} : ${hn[i].innerText.length} caractères</span>`;
-    hn[i].appendChild(hnLength);
-  }
-  var hn = document.getElementsByTagName('h6');
-  for (var i = 0; i < hn.length; i++) {
-    var hnLength = document.createElement('div');
-    hnLength.innerHTML = `<span style="color:red;background:black;padding:15ps">${hn[i].tagName} : ${hn[i].innerText.length} caractères</span>`;
-    hn[i].appendChild(hnLength);
-  }
-})()
+  const allHnElements = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
+  allHnElements.forEach(function(element) {
+    element.innerHTML = `${element.innerHTML} <span style="color:red;background:black;padding:15ps">Niveau : ${element.tagName} - Nombre de caractères : ${element.innerText.length}</span>`;
+  });
+})();
