5a6bb373eb30235d783fb8134ff8ce3ec846ed5d
Artkabis <artkabis@hotmail.fr>
2023-01-30 16:09:59 +0100

    Update : refactoring app

--- Solocal tools, tips & fix/tools-cq-checker/checkImgSizeAndAlt.js
+++ Solocal tools, tips & fix/tools-cq-checker/checkImgSizeAndAlt.js
@@ -1,60 +1,46 @@
 javascript:(()=>{
-    function formatBytes(bytes) {
-        if (bytes < 1024)
-            return bytes + " Bytes";
-        else if (bytes < 1048576)
-            return (bytes / 1024).toFixed(2) + " KB";
-        else if (bytes < 1073741824)
-            return (bytes / 1048576).toFixed(2) + " MB";
-        else
-            return (bytes / 1073741824).toFixed(2) + " GB"
-    }
-    function checkUrlImg(url,alt,type){
-        var req = new XMLHttpRequest();
-        req.open('GET', url, !0);
-        req.send();
-        req.onreadystatechange = function() {
-            if (this.readyState == this.HEADERS_RECEIVED) {
-                var headers = req.getAllResponseHeaders();
-                var arr = headers.trim().split(/[\r\n]+/);
-                var map = {};
-                arr.forEach(function(line) {
-                    var parts = line.split(': ');
-                    var header = parts.shift();
-                    var value = parts.join(': ');
-                    map[header] = value
-                });
-                fsize = map['content-length'];
-                if (fsize != null) {
-                    console.table({url : url,'size':formatBytes(fsize),'alt':alt,type:type})
-                } else {
-                    console.warn("Not available")
-                }
-            }
-        }
+  function formatBytes(bytes) {
+      return bytes < 1024
+        ? `${bytes} Bytes`
+        : bytes < 1048576
+        ? `${(bytes / 1024).toFixed(2)} KB`
+        : bytes < 1073741824
+        ? `${(bytes / 1048576).toFixed(2)} MB`
+        : `${(bytes / 1073741824).toFixed(2)} GB`;
     }
 
+    async function checkUrlImg(url, alt = '', type) {
+      try {
+        const response = await fetch(url, { method: 'HEAD' });
+        const headers = response.headers;
+        const size = headers.get('content-length');
+        console.table({ url, size: formatBytes(size), alt, type });
+      } catch (error) {
+        console.warn('Not available');
+      }
+    }
 
+    const images = [...document.querySelectorAll('img')];
+    images.forEach(({ src, alt }) => {
+      src && checkUrlImg(src, alt, 'srcImage');
+    });
 
-    document.querySelectorAll('img').forEach(function(t,i){
-        (t && t.src)&&checkUrlImg(t.src,t.getAttribute('alt'),'srcImage');
+    const bgImages = [...document.querySelectorAll('div')].filter(
+      ({ style: { backgroundImage } }) => backgroundImage
+    );
+    bgImages.forEach(({ style: { backgroundImage } }) => {
+      let bgimg = backgroundImage.split('url("')[1].split('")')[0];
+      bgimg = bgimg.includes('http') ? bgimg : `${window.location.origin}${bgimg}`;
+      checkUrlImg(bgimg, 'no alt -> gbimg', 'bgImage');
     });
-    
-    document.querySelectorAll('div').forEach(function(t,i){
-        if(t.style.backgroundImage){
-            let bgimg = t.style.backgroundImage.split('url("')[1].split('")')[0];
-            bgimg = (bgimg.includes('http')) ? bgimg : window.location.origin +bgimg;
-            (bgimg)&&checkUrlImg(bgimg,'no alt -> gbimg','bgImage')
-        }
-    });
-    (function($){
-        const element = $('div[class*="vc_custom_"]');
-        element.each(function(i,t){
-            if($(this).css('background-image') && String($(this).css('background-image'))!== 'none'){
-                let bgimg = String($(this).css('background-image')).split('url("')[1].split('")')[0];
-                bgimg = (bgimg.includes('http')) ? bgimg : window.location.origin +bgimg;
-                (bgimg)&&checkUrlImg(bgimg,'no alt -> gbimg','bgImage')
-            }
+
+    $(() => {
+      $('div[class*="vc_custom_"]')
+        .filter(({ style: { 'background-image': bgImage } }) => bgImage !== 'none')
+        .forEach(({ style: { 'background-image': bgImage } }) => {
+          let bgimg = bgImage.split('url("')[1].split('")')[0];
+          bgimg = bgimg.includes('http') ? bgimg : `${window.location.origin}${bgimg}`;
+          checkUrlImg(bgimg, 'no alt -> gbimg', 'bgImage');
         });
-    })(jQuery);
+    });
 })();
