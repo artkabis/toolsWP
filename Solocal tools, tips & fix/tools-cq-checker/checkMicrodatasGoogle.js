bf056b48b4f07ab4654ca5f5a748c866ed87108b
Artkabis <artkabis@hotmail.fr>
2023-03-23 11:33:10 +0100

    Create checkMicrodatasGoogle.js

---
+++ Solocal tools, tips & fix/tools-cq-checker/checkMicrodatasGoogle.js
@@ -0,0 +1,1 @@
+javascript:(()=>window.open('https://search.google.com/test/rich-results?utm_source=support.google.com%2Fwebmasters%2F&utm_medium=referral&utm_campaign=7445569&url=%27+encodeURIComponent(window.location.href),%27_blank%27))()
