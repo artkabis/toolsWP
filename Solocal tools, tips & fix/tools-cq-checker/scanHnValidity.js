f2ae93ca587038c2c1d4ff9841a3e68a8fd5eb7a
Artkabis <artkabis@hotmail.fr>
2023-04-13 15:47:54 +0200

    Update scanHnValidity.js

--- Solocal tools, tips & fix/tools-cq-checker/scanHnValidity.js
+++ Solocal tools, tips & fix/tools-cq-checker/scanHnValidity.js
@@ -1,9 +1,9 @@
 javascript:(()=>{
     document.querySelectorAll('h1,h2,h3,h4,h5,h6').forEach((t,i)=>{
         const nbLetters = t.textContent.length;
-        const tagName = t.tagName
-        const tagContent = t.textContent
-        console.table({[tagName]:tagContent,' nb word':nbLetters})
-        if((tagName === 'H1' || tagName === "H2") && nbLetters < 50 || nbLetters > 90){console.log('%c'+tagName+' : '+tagContent+'  Erreur -> nombre de caractère : '+nbLetters+', ne rentre pad dans le ration 50 -> 90','color:red',)}        
+        const tagName = t.tagName;
+        const tagContent = t.textContent;
+        console.table({[tagName]:tagContent,' nb word':nbLetters});
+        if((tagName === 'H1' || tagName === "H2") && nbLetters < 50 || nbLetters > 90){console.log('%c'+tagName+' : '+tagContent+'  ------ Erreur -> nombre de caractère : '+nbLetters+', ne rentre pad dans le ration 50 -> 90','color:red',)}        
     })
 })()
