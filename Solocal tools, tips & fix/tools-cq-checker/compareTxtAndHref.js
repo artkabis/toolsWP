f6cf4a0e5dffecded2a2a8bacde446c5c2e53bac
Artkabis <artkabis@hotmail.fr>
2023-04-14 10:31:47 +0200

    Update compareTxtAndHref.js

--- Solocal tools, tips & fix/tools-cq-checker/compareTxtAndHref.js
+++ Solocal tools, tips & fix/tools-cq-checker/compareTxtAndHref.js
@@ -3,8 +3,7 @@ javascript:(()=>{
         let infosLinks = [];
         $('#Content a, #site_content a').each(function(i, t) {
             const href = $(this).attr('href');
-            const txt = $(this)[0].textContent;
-            console.log(href);
+            const txt = $(this)[0].innerText;
             const isAncor = href ? href.includes('#') : false;
             const linkId = `link-${i + 1}`;
             infosLinks.push({
