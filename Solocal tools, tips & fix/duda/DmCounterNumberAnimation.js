c3627eacd43705f0cb27eef9fb8e7afa31ed3bea
Artkabis <artkabis@hotmail.fr>
2023-04-18 14:37:00 +0200

    Update DmCounterNumberAnimation.js

--- Solocal tools, tips & fix/duda/DmCounterNumberAnimation.js
+++ Solocal tools, tips & fix/duda/DmCounterNumberAnimation.js
@@ -1,69 +1,55 @@
-
  dmAPI.runOnReady('DudaReadyDOM',funcReadyDomDM);
   function funcReadyDomDM(){
       const env = dmAPI.getCurrentEnvironment();
-      if(env !== 'editor') {
-          const blocCounter = document.querySelector(".blocCounter");//blocCounter qui devront être détectés pour lancer l'animation
-        if(blocCounter){   
+      //if(env !== 'editor') {
+          //console.log(document.querySelector(".blocCounter"),document.querySelector(".blocCounter").length);
+          const blocCounter = document.querySelector(".blocCounter");
+        if(blocCounter){
+            
             let blocCounterValue = blocCounter.getBoundingClientRect();
             const counters = document.querySelectorAll(".numberCounter");
             // LES PARAMETRES DE BASE DE L'ANIMATION
             let play = false;
-            let duration = 3000; // Temps de l'animation en millisecondes
+            let duration = 2000; // temps de l'animation en millisecondes
             
             // LE DÉCLENCHEUR DE L'ANIMATION = ici 1/2 de la Hauteur de l'écran (paramétrable)
             const heightDivide = 2;
             window.addEventListener("scroll", (event) => {
-              let halfWindow = window.scrollY + window.innerHeight / heightDivide;
-              if (blocCounterValue.y < halfWindow && !play) {
-                animCounter(counters,duration,play);
-                console.log("play");
-                play = !play;
-              }
+            let halfWindow = window.scrollY + window.innerHeight / heightDivide;
+            if (halfWindow  > $('.blocCounter').offset().top && !play) {
+            animCounter(counters,duration,play);
+            console.log("play");
+            play = !play;
+            }
             });
             // LE COEUR DE L'ANIMATION
             const animCounter = (counters,duration,play) => {
-              counters.forEach((counter) => {
-                const value = Number(counter.getAttribute("data-target"));
-                let startValue = 0;
-                let starttime = null;
-                function animate(timestamp) {
-                  if (!starttime) {
-                    starttime = timestamp;
-                  }
-                  const runtime = timestamp - starttime;
-                  const relativeProgress = runtime / duration;
-                  if (timestamp !== undefined) {
-                    startValue = Math.trunc(value * Math.min(relativeProgress, 1));
-                  }
-                  counter.textContent = Number(startValue);
-                  if (runtime < duration) {
-                    requestAnimationFrame(animate);
-                  }
-                }
-                requestAnimationFrame(animate);
-                animate();
-              });
+            counters.forEach((counter) => {
+            const value = Number(counter.getAttribute("data-target"));
+            let startValue = 0;
+            let starttime = null;
+            function animate(timestamp) {
+            if (!starttime) {
+            starttime = timestamp;
+            }
+            const runtime = timestamp - starttime;
+            const relativeProgress = runtime / duration;
+            if (timestamp !== undefined) {
+            startValue = Math.trunc(value * Math.min(relativeProgress, 1));
+            }
+            counter.textContent = Number(startValue);
+            if (runtime < duration) {
+            requestAnimationFrame(animate);
+            }
+            }
+            requestAnimationFrame(animate);
+            animate();
+            });
             };
         }
-     }
+
+     //}
   }
 
+  
 
-/******* HTML brut -->
-
-
-
-<div class="blocCounter">
-  <span class="prefixe"></span>
-  <div class="numberCounter" data-target="6360">0</div>
-  <span class="suffixe"></span>
-</div>
-
-
-CSS -->
-.numberCounter {color:#333; font-size:48px;padding-bottom:20px;font-weight: 700;display: flex;justify-content: center;flex-direction: column;align-items: center;}
-.numberCounter:after {content: '';display: block;background-color: orange;height: 4px;width:50px;} 
-.suffixe {color:#fff;}
-
-*******************/
