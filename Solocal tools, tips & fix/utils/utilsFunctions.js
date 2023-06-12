33f3d147b2dafe5c26e6a948de16336b1470b007
Artkabis <artkabis@hotmail.fr>
2023-06-12 17:58:14 +0200

    Update utilsFunctions.js

--- Solocal tools, tips & fix/utils/utilsFunctions.js
+++ Solocal tools, tips & fix/utils/utilsFunctions.js
@@ -9,3 +9,25 @@ const countChange = (money, coins) =>{
   }
   return ways[money];
 }
+
+
+//FSM to TCP
+function traverseTCPStates(eventList){
+  const state = "CLOSED";   
+  const states = { 
+    CLOSED : { APP_PASSIVE_OPEN : "LISTEN", APP_ACTIVE_OPEN : "SYN_SENT" },
+    LISTEN: { RCV_SYN : "SYN_RCVD", APP_SEND : "SYN_SENT", APP_CLOSE : "CLOSED" },
+    SYN_RCVD: { APP_CLOSE : "FIN_WAIT_1", RCV_ACK : "ESTABLISHED" },
+    SYN_SENT: { RCV_SYN : "SYN_RCVD", RCV_SYN_ACK : "ESTABLISHED", APP_CLOSE : "CLOSED" },
+    ESTABLISHED: { APP_CLOSE : "FIN_WAIT_1", RCV_FIN : "CLOSE_WAIT" },
+    FIN_WAIT_1: { RCV_FIN : "CLOSING", RCV_FIN_ACK : "TIME_WAIT", RCV_ACK : "FIN_WAIT_2" },
+    CLOSING: { RCV_ACK : "TIME_WAIT" },
+    FIN_WAIT_2: { RCV_FIN : "TIME_WAIT" },
+    TIME_WAIT: { APP_TIMEOUT : "CLOSED" },
+    CLOSE_WAIT: { APP_CLOSE : "LAST_ACK" },
+    LAST_ACK: { RCV_ACK : "CLOSED" },
+    ERROR: {}
+  };
+
+  return eventList.reduce(function(state, input) { return states[state][input] || "ERROR"; }, state);
+}
