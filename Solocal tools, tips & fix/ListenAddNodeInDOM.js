aa0854f5fec439a6b18dcfbfda20ad057a35059f
Artkabis <artkabis@hotmail.fr>
2023-09-08 11:32:39 +0200

    Add disconnect  observer if element does not  exist

--- Solocal tools, tips & fix/ListenAddNodeInDOM.js
+++ Solocal tools, tips & fix/ListenAddNodeInDOM.js
@@ -1,19 +1,24 @@
+
 // Gestion de la mutation du DOM
 const checkDOMMutation = (classListen, utilsFunction) => {
+  let nodesToObserve = document.querySelectorAll("." + classListen).length; // Nombre total d'éléments à observer
   const handleMutation = (mutationsList, observer) => {
     for (const mutation of mutationsList) {
       if (mutation.type === "childList") {
         const addedElements = Array.from(mutation.addedNodes);
-        const privacyPolicyTextElement = addedElements.find((node) => {
+        addedElements.forEach((node) => {
           if (String(node.classList).includes(classListen)) {
             utilsFunction();
-            observer.disconnect();
+            nodesToObserve--; // Marque l'élément comme "traité"
           }
         });
       }
     }
+    // Si tous les éléments ont été observés et traités, arrêtez l'observation.
+    if (nodesToObserve === 0) {
+      observer.disconnect();
+    }
   };
-
   const parentElement = document.body;
   const observer = new MutationObserver(handleMutation);
   const observerOptions = {
@@ -22,11 +27,17 @@ const checkDOMMutation = (classListen, utilsFunction) => {
   };
   observer.observe(parentElement, observerOptions);
 };
-// Remplacement d'une adresse mail qui n'est plus valide en BDD
-const replaceMail = ()=>{
-  $(".woocommerce-privacy-policy-text").html(String($(".woocommerce-privacy-policy-text").html()).replace(new RegExp(/guichard.berty@wanadoo.fr/g),'magasin@espace-motoculture-chauvigny.fr'));
+
+// Remplacement d'une adresse e-mail qui n'est plus valide en BDD
+const replaceMail = () => {
+  $(".woocommerce-privacy-policy-text").html(
+    String($(".woocommerce-privacy-policy-text").html()).replace(
+      new RegExp(/guichard.berty@wanadoo.fr/g),
+      "magasin@espace-motoculture-chauvigny.fr"
+    )
+  );
 };
-//Sur la page commande, on écoute l'ajout de woocommerce-checkout, si celui-ci arrive dans le DOM, nous remplaçons l'adresse mail par la bonne.
-window.location.pathname.includes('command') && checkDOMMutation(
-  "woocommerce-checkout-payment",replaceMail
-);
+
+// Sur la page commande, on écoute l'ajout de woocommerce-checkout, si celui-ci arrive dans le DOM, nous remplaçons l'adresse e-mail par la bonne.
+window.location.pathname.includes("command") &&
+  checkDOMMutation("woocommerce-checkout-payment", replaceMail);// On écoute la mutation de la classe woocommerce-checkout-payment
