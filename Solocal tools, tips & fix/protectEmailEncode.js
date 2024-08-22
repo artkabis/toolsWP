4ec164a733676b715b5f59f46aad7836ee0b48f9
Artkabis <artkabis@hotmail.fr>
2024-08-22 14:45:54 +0200

    Update protectEmailEncode.js
    Add menu li Wordpress <a> mailto

--- Solocal tools, tips & fix/protectEmailEncode.js
+++ Solocal tools, tips & fix/protectEmailEncode.js
@@ -6,9 +6,8 @@
 
 
 (function($){
-    function reverseEmail($mail){
-        return ($mail) && $mail.split("").reverse().join("")
-    }
+    const reverseEmail = ($mail) => ($mail) && $mail.split("").reverse().join("");
+
     //Ceci permet de récupérer les adresses mails inversé (avant intégration). 
     //Il faudra supprimer cette méthode qui ne sert qu'à convertir vos/votre adresse mail.
     $.each(["monadress1@gmail.com","monadress2@gmail.com","monadress3@gmail.com"],function(i,t){
@@ -25,3 +24,30 @@
         location.href = "mailto:"+mailReverse+"?cc=lemailencopie@gmail.com&subject=Prise de contact depuis votre site monsite.com&body=Ce mail concerne votre site internet";
     });
 })(jQuery)
+
+
+/************** Si vous avait aussi une adresse mail en lien avec le menu donc vous ne pourrez ajouter la classe encodeMail uniquement sur le <li> et non le <a> 
+
+Voici un script qui permet d'identifier les deux et de les gérer séparément ->
+***/
+
+/**** DOM html qui sera traité par le script >>
+LE A : <a class="encodemail">rf.oodanaw@2leinad.tohcorb</a>
+LE LI : <li class="encodemail"><a href="mailto:rf.oodanaw@2leinad.tohcorb">Email</a></li>
+**/
+const reverseEmail = ($mail) => $mail && $mail.split("").reverse().join("");
+ 
+//Les e-mails seront désormais visibles dans leur format initial, sans que ce soit le cas dans le DOM
+$('.encodemail').each(function(i,t){
+    const isA = $(this)[0].nodeName == 'A'  ? true : false;
+    (isA) && $(this).css({unicodeBidi:"bidi-override",direction:"rtl",cursor:"pointer"});
+});
+
+//Reconstruction de l'e-mail uniquement après click, puis ouverture du mailto
+$('.encodemail').on('click',function(){
+    const isA = $(this)[0].nodeName == 'A'  ? true : false;
+    const mailReverse = isA ? reverseEmail($(this).text()) : reverseEmail($(this).find('a').attr('href').split('mailto:')[1]);
+    window.location.href = "mailto:"+mailReverse+"?&subject=Prise de contact depuis votre site sarlbrochot.fr";
+});
+ 
+
